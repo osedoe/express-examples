3f4310fbb5380f37a6202c2145acf17de8c3d5b9
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-11-12 10:33:50 +0000

    Add general GET endpoint for movies

--- server.js
+++ server.js
@@ -4,44 +4,93 @@ const app = express();
 
 app.use(express.json());
 
-function readMovies() {
+const moviesPath = `model/movies.json`;
+const usersPath = `model/users.json`;
 
+let movies;
+let users;
+
+// Functions to load files
+function loadMovies(moviesFile) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(moviesFile, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                movies = JSON.parse(data);
+                resolve(movies);
+            }
+        });
+    });
 }
-fs.readFile('movies.json');
+
+function loadUsers(usersFile) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(usersFile, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                users = JSON.parse(data);
+                resolve(users);
+            }
+        })
+    });
+}
 
 // Endpoints
-const users = [{
-        id: "1",
-        name: "Pepe"
-    },
-    {
-        id: "2",
-        name: "Juan"
-    }
-];
+app.get('/api/movies', (req, res) => {
+    res.json(movies);
+});
 
-app.get("/users", (req, res) => {
+
+app.get("/api/users", (req, res) => {
     // console.log('Request:', req);
     // res.send('Hello World!');
+    console.log(req);
     res.json(users);
 });
 
-// app.get("/users/:id", (req, res) => {
-//     const userId = req.params.id;
-//     const user = users.find(user => user.id === userId);
-//     res.json(user);
-// });
+app.get('/api/movies/:query', (req, res) => {
+    let query = req.params.query.toLowerCase().replace(/\+/, ' ');
+    let [key, value] = query.split('=');
+    if (key === 'id' || key === 'title' || key === 'director' || key === 'likes') {
+        const movie = movies.find(movie => movie[key] === value);
+        if (movie == undefined) {
+            res.json(`Movie not found.`);
+        }
+        res.json(movie);
+    } else {
+        res.json(`Search parameter may be incorrect.`);
+    }
+});
 
-app.post("/users/:username", (req, res) => {
+app.get('/api/movies/:title', (req, res) => {
+    const movieTitle = req.params.title;
+    const movie = movies.find(movie => movie.title === movieTitle);
+    res.json(movie);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const userId = req.params.id;
+    const user = users.find(user => user.id === userId);
+    res.json(user);
+});
+
+app.post("/api/users/:username", (req, res) => {
     const newUser = req.body;
-    console.log(newUser);
+    // console.log(newUser);
     if (!newUser || newUser !== undefined || newUser !== null) {
         res.status(400).send("Debes pasarme algo en el Body");
     } else {
         newUser.id = Math.random();
+        newUser.password = Math.random() * 9999 + 1111;
         users.push(newUser);
         res.json(newUser);
     }
 });
 
-app.listen(3000, () => console.log("Ready on port 3000"));
+app.listen(3000, () => {
+    movies = loadMovies(moviesPath).then(() => console.log(`Movies file has been read.`));
+    users = loadUsers(usersPath).then(() => console.log(`Users file has been read.`));
+    console.log("Ready on port 3000");
+});
