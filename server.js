eadafdeb95922df2f81beeddc57929f59d5c89ef
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-11-12 08:39:42 +0000

    Move json files to model/

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
-const express = require('express');
+const express = require("express");
+const fs = require('fs');
 const app = express();
+
 app.use(express.json());
 
+function readMovies() {
+
+}
+fs.readFile('movies.json');
 
 // Endpoints
 const users = [{
-    id: '1',
-    name: 'Pepe'
-}, {
-    id: '2',
-    name: 'Juan'
-}];
+        id: "1",
+        name: "Pepe"
+    },
+    {
+        id: "2",
+        name: "Juan"
+    }
+];
 
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
     // console.log('Request:', req);
     // res.send('Hello World!');
     res.json(users);
 });
 
-app.get('/users/:id', (req, res) => {
-    const userId = req.params.id;
-    const user = users.find(user => user.id === userId);
-    res.json(user);
-});
+// app.get("/users/:id", (req, res) => {
+//     const userId = req.params.id;
+//     const user = users.find(user => user.id === userId);
+//     res.json(user);
+// });
 
-app.post('/users', (req, res) => {
+app.post("/users/:username", (req, res) => {
     const newUser = req.body;
+    console.log(newUser);
     if (!newUser || newUser !== undefined || newUser !== null) {
-        res.status(400).send('Debes pasarme algo en el Body');
+        res.status(400).send("Debes pasarme algo en el Body");
     } else {
         newUser.id = Math.random();
         users.push(newUser);
         res.json(newUser);
     }
-
 });
 
-
-
-app.listen(3000, () => console.log('Ready on port 3000'));
+app.listen(3000, () => console.log("Ready on port 3000"));
